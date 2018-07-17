480458725b72be251670112359e47f74b0e96c4f
owen51251 <30658163+owen51251@users.noreply.github.com>
2018-07-17 09:12:58 +0800

    Update autologin_gmail.py

--- autologin_gmail.py
+++ autologin_gmail.py
@@ -13,13 +13,13 @@ time.sleep(2)
 
 account = dr.find_element_by_xpath('//*[@id="identifierId"]')
 account.clear()
-account.send_keys("owen51251")
+account.send_keys("帳號")
 dr.find_element_by_xpath('//*[@id="identifierNext"]/content/span').click()
 time.sleep(2)
 
 password = dr.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input')
 password.clear()
-password.send_keys("sam51251")
+password.send_keys("密碼")
 dr.find_element_by_xpath('//*[@id="passwordNext"]/content/span').click()
 
 #dr.quit()
